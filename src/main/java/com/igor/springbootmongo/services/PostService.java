20e34f9ea3f7a789a6e8f1ea37ff8d8fe8efda78
Igor <igordamiano@gmail.com>
2020-04-20 18:16:31 -0300

    Post por ID

---
+++ src/main/java/com/igor/springbootmongo/services/PostService.java
@@ -0,0 +1,26 @@
+package com.igor.springbootmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.igor.springbootmongo.domain.Post;
+import com.igor.springbootmongo.repository.PostRepository;
+import com.igor.springbootmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	
+	public Post findById(String id) {
+	 	Optional<Post> obj = repo.findById(id);
+
+	 	// vai dar um get no Post, se não tiver, vai lançar a exceção
+	 	return obj.orElseThrow(() -> new ObjectNotFoundException(id));
+	}
+	
+}
