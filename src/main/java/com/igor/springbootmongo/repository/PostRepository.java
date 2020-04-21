a9656efe0be13c9fa638d8af9c578d578bbcc669
Igor <igordamiano@gmail.com>
2020-04-21 16:06:31 -0300

    Consulta simples com @Query

--- src/main/java/com/igor/springbootmongo/repository/PostRepository.java
+++ src/main/java/com/igor/springbootmongo/repository/PostRepository.java
@@ -3,12 +3,18 @@ package com.igor.springbootmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.igor.springbootmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
+	
+	//https://docs.mongodb.com/manual/reference/operator/query/regex/
+	//@Query("{ <field>: { $regex: /pattern/ ?0 é o parametro text, $options: '<options>' i=ignora maiusculas e minusculas } }")
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 
